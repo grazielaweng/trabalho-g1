5608dce26b71346f0eb25a48939a8c85c845dd02
grazielaweng <125660385+grazielaweng@users.noreply.github.com>
2023-11-10 10:50:33 -0300

    x
    de

---
+++ função.js
@@ -0,0 +1,11 @@
+function LeiaSeuNome(autor){
+    if (autor == "Graziela"){
+        LeiaSeuNome("Ohana quer dizer familia.familia quer dizer nunca abandonar ou esquecer")
+    }
+}
+const autor["Graziela"];
+let contador =0;
+while(contador< autor.length){
+    LeiaSeuNome(autor[contador]);
+    contador=contador+1;
+}
